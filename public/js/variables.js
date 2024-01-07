7afa480e26ef4d5b35322c087dba6f6af66505d7
Angela <antunezangela89@gmail.com>
2024-01-07 23:10:56 +0100

    Finish exercise 1 variables

---
+++ public/js/variables.js
@@ -0,0 +1,15 @@
+/* 1-Crear variables de diferentes tipos de datos: número, cadena de texto, booleano e imprimir su valor por
+la pantalla del navegador y la consola.*/
+
+let number = 1989;
+let name = "Ángela";
+let data = true;
+console.log(`${name} was born in ${number} and this is ${data}.`);
+let answer1 = document.getElementById('answer-1');
+answer1.innerHTML = `${name} was born in ${number} and this is ${data}.`
+
+/* 2-De las variables declaradas anteriormente, identifica las que son de tipo integer y realiza operaciones matemáticas básicas con las variables creadas anteriormente (suma, resta, multiplicación, división). Deberás imprimir el resultado por la pantalla del navegador y la consola.*/
+
+
+
+/*3-De las variables declaradas anteriormente, identifica las que son de tipo texto y concatena dichas cadenas de texto.Deberás imprimir el resultado por la pantalla del navegador y la consola.*/
