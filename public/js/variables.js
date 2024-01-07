e9c692790f8247ba40ecc953e652e036181719bc
Angela <antunezangela89@gmail.com>
2024-01-08 00:42:04 +0100

    Improve exercise 1 variables

--- public/js/variables.js
+++ public/js/variables.js
@@ -1,15 +1,20 @@
 /* 1-Crear variables de diferentes tipos de datos: número, cadena de texto, booleano e imprimir su valor por
 la pantalla del navegador y la consola.*/
 
-let number = 1989;
+let dateBorn = 1989;
+let dateCoding = 2023;
+let age = 34;
 let name = "Ángela";
-let data = true;
-console.log(`${name} was born in ${number} and this is ${data}.`);
+let hometown = "Santander"
+let currentCity = "Oviedo";
+let dogs = true;
+let cats = false;
+console.log(`${name} was born in ${dateBorn} in ${hometown}, but now she lives in ${currentCity}. She is ${age} and she decided to start learning web development in ${dateCoding}. A ${dogs} fact: she has two dogs. She always thought that cats were ${cats} but now she would like to have one.`);
 let answer1 = document.getElementById('answer-1');
-answer1.innerHTML = `${name} was born in ${number} and this is ${data}.`
+answer1.innerHTML = `${name} was born in ${dateBorn} in ${hometown}, but now she lives in ${currentCity}. She is ${age} and she decided to start learning web development in ${dateCoding}. A ${dogs} fact: she has two dogs. She always thought that cats were ${cats} but now she would like to have one.`;
 
 /* 2-De las variables declaradas anteriormente, identifica las que son de tipo integer y realiza operaciones matemáticas básicas con las variables creadas anteriormente (suma, resta, multiplicación, división). Deberás imprimir el resultado por la pantalla del navegador y la consola.*/
 
 
 
-/*3-De las variables declaradas anteriormente, identifica las que son de tipo texto y concatena dichas cadenas de texto.Deberás imprimir el resultado por la pantalla del navegador y la consola.*/
+/*3-De las variables declaradas anteriormente, identifica las que son de tipo texto y concatena dichas cadenas de texto. Deberás imprimir el resultado por la pantalla del navegador y la consola.*/
