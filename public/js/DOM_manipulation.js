8a12e206fbb136ab3ba9a65ceb6cef02f3b8630d
Angela <antunezangela89@gmail.com>
2024-01-10 22:56:13 +0100

    Finish exercise 7-9 for DOM Manipulation

--- public/js/DOM_manipulation.js
+++ public/js/DOM_manipulation.js
@@ -73,8 +73,141 @@ howManyNumbers();
 
 /*6-Create the nodes required to print a form.*/
 
+const form = document.createElement('form');
+
+form.style.display = "flex";
+
+form.style.flexDirection = "column";
+
+form.style.maxWidth = "300px";
+
+const input = document.createElement('input');
+
+const button = document.createElement('button');
+
+button.innerText = "Send";
+
+form.appendChild(input);
+
+form.appendChild(button);
+
+const answer6 = document.getElementById('answer-6');
+
+answer6.appendChild(form);
+
 /*7-Create the nodes required to print a table.*/
+
+const table = document.createElement('table');
 
 /*8-Create an array of 10 objects with their pairs of key-values. Every object must have the following keys: id, name, status, species, type, gender. It's up to you to assign the corresponding value for each key.*/
 
+const animals = [
+    {
+        id: 1,
+        name: "Pepito",
+        status: "dead",
+        species: "cricket",
+        type: "green",
+        gender: "fluid"
+    },
+    {
+        id: 2,
+        name: "Lía",
+        status: "fat",
+        species: "cat",
+        type: "sleepy",
+        gender: "mademoiselle"
+    },
+    {
+        id: 3,
+        name: "Rocky",
+        status: "jamon-addicted",
+        species: "dog",
+        type: "barky",
+        gender: "vicious"
+    },
+    {
+        id: 4,
+        name: "Sira",
+        status: "ball-obsessed",
+        species: "dog",
+        type: "cute",
+        gender: "lady"
+    },
+    {
+        id: 5,
+        name: "Paco",
+        status: "nervous",
+        species: "dog",
+        type: "kayak-rider",
+        gender: "unknown"
+    },
+    {
+        id: 6,
+        name: "Valerius",
+        status: "learning",
+        species: "human",
+        type: "dj",
+        gender: "male"
+    },
+    {
+        id: 7,
+        name: "Antoñanzas",
+        status: "programming",
+        species: "robot",
+        type: "pacient",
+        gender: "sexy"
+    },
+    {
+        id: 8,
+        name: "Jesucrista",
+        status: "eager to help",
+        species: "drama queen",
+        type: "needy",
+        gender: "sexy"
+    },
+    {
+        id: 9,
+        name: "Robert",
+        status: "solving the next big integral",
+        species: "retired",
+        type: "duo-lingo-addicted",
+        gender: "male"
+    },
+    {
+        id: 10,
+        name: "Marcelita",
+        status: "cleaning",
+        species: "retired",
+        type: "10.000 steps maker",
+        gender: "madame"
+    }
+]
+
 /*9-Print each element of the object inside the table you previously created.*/
+
+
+let tableContent = "<tr>";
+
+for (const key in animals[0]) {
+    tableContent += `<th>${key}</th>`;
+}
+
+tableContent += "</tr>";
+
+for (const animal of animals) {
+    tableContent += "<tr>";
+
+    for (const key in animal) {
+        tableContent += `<td>${animal[key]}</td>`;
+    }
+
+    tableContent += "</tr>";
+
+}
+
+table.innerHTML = tableContent;
+
+const answer7_9 =document.getElementById('answer-7-9');
+
+answer7_9.appendChild(table);
